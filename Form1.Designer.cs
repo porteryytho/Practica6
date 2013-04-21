f700af82b4508799689d82306bfeab97e9ccbb9f
porteryytho <betoesportero_hola@hotmail.com>
2013-04-21 16:28:07 -0500

    programa que saca la factura y limpia la pantalla

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Practica6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.prod1 = new System.Windows.Forms.TextBox();
+            this.sub = new System.Windows.Forms.TextBox();
+            this.iva = new System.Windows.Forms.TextBox();
+            this.total = new System.Windows.Forms.TextBox();
+            this.prod2 = new System.Windows.Forms.TextBox();
+            this.prod3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(111, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Factura";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(200, 326);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 28);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Limpiar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "1° producto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "2° Producto";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "3° Producto";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "SubTotal";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 200);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "IVA";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 229);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Total";
+            // 
+            // prod1
+            // 
+            this.prod1.Location = new System.Drawing.Point(95, 31);
+            this.prod1.Name = "prod1";
+            this.prod1.Size = new System.Drawing.Size(169, 20);
+            this.prod1.TabIndex = 8;
+            // 
+            // sub
+            // 
+            this.sub.Location = new System.Drawing.Point(95, 170);
+            this.sub.Name = "sub";
+            this.sub.Size = new System.Drawing.Size(169, 20);
+            this.sub.TabIndex = 9;
+            // 
+            // iva
+            // 
+            this.iva.Location = new System.Drawing.Point(95, 197);
+            this.iva.Name = "iva";
+            this.iva.Size = new System.Drawing.Size(169, 20);
+            this.iva.TabIndex = 10;
+            // 
+            // total
+            // 
+            this.total.Location = new System.Drawing.Point(95, 226);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(169, 20);
+            this.total.TabIndex = 11;
+            // 
+            // prod2
+            // 
+            this.prod2.Location = new System.Drawing.Point(95, 60);
+            this.prod2.Name = "prod2";
+            this.prod2.Size = new System.Drawing.Size(169, 20);
+            this.prod2.TabIndex = 13;
+            // 
+            // prod3
+            // 
+            this.prod3.Location = new System.Drawing.Point(95, 92);
+            this.prod3.Name = "prod3";
+            this.prod3.Size = new System.Drawing.Size(169, 20);
+            this.prod3.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 372);
+            this.Controls.Add(this.prod3);
+            this.Controls.Add(this.prod2);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.iva);
+            this.Controls.Add(this.sub);
+            this.Controls.Add(this.prod1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox prod1;
+        private System.Windows.Forms.TextBox sub;
+        private System.Windows.Forms.TextBox iva;
+        private System.Windows.Forms.TextBox total;
+        private System.Windows.Forms.TextBox prod2;
+        private System.Windows.Forms.TextBox prod3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Practica6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double prod1 = double.Parse(this.prod1.Text);
+            double prod2 = double.Parse(this.prod3.Text);
+            double prod3 = double.Parse(this.prod3.Text);
+
+            double sub = prod1 + prod2 + prod3;
+            this.sub.Text = sub.ToString();
+
+            double iva = sub * .16;
+            this.iva.Text = iva.ToString();
+
+            double total = iva + sub;
+            this.total.Text = total.ToString();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            prod1.Clear();
+            prod2.Clear();
+            prod3.Clear();
+            sub.Clear(); 
+            iva.Clear();
+            total.Clear();
+        }
+    }
+}
